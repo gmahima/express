81bc17ce0c5b1a9d31c13fb015ce3f64c1770314
mahima gangavarapu <mahimagangavarapu@gmail.com>
2019-02-03 12:16:16 +0530

    show hint only when ques is asked

--- routes/cards.js
+++ routes/cards.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router()
 const {data} = require('../data/flashcardData.json');
 const {cards} = data;
+let notSide;
 router.get('/:id', (req, res) => {
   const {side} = req.query;
+  if (side =='question') {
+   notSide = 'answer';
+  }
+  else {notSide = 'question';}
   const {id} = req.params;
   const text = cards[id][side];
   const {hint} = cards[id];
-  const templateData = {text}
+  const templateData = {text, id, side, notSide}
   if (side == 'question') {
     templateData.hint = hint;
   }
