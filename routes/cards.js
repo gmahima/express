c890a82e232751c4245e4889f73ce6ec92ad7378
mahima gangavarapu <mahimagangavarapu@gmail.com>
2019-02-03 14:46:27 +0530

    add default behaviour

--- routes/cards.js
+++ routes/cards.js
@@ -2,17 +2,26 @@ const express = require('express');
 const router = express.Router()
 const {data} = require('../data/flashcardData.json');
 const {cards} = data;
-let notSide;
+const cardsLength = cards.length;
+console.log(Math.floor(Math.random()*cardsLength));
+//random card generator
+router.get('/', (req,res) => {
+const defaultSide = 'question';
+const randomId = Math.floor(Math.random()*cardsLength);
+res.redirect(`/cards/${randomId}?side=${defaultSide}`);
+});
+// specific id route
 router.get('/:id', (req, res) => {
   const {side} = req.query;
-  if (side =='question') {
-   notSide = 'answer';
-  }
-  else {notSide = 'question';}
+
   const {id} = req.params;
   const text = cards[id][side];
   const {hint} = cards[id];
-  const templateData = {text, id, side, notSide}
+  const templateData = {text, id, side}
+  if (side =='answer') {
+   templateData.notSide = 'question';
+  }
+  else {templateData.notSide = 'answer';}
   if (side == 'question') {
     templateData.hint = hint;
   }
