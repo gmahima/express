b67d33bf1db56fcdc883d0478c757e9d5eb572c0
mahima gangavarapu <mahimagangavarapu@gmail.com>
2019-02-03 11:40:43 +0530

    add query strings

--- routes/cards.js
+++ routes/cards.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router()
-router.get('/', (req, res) => {
-  res.render('card', {prompt: 'java == javaScript', hint: "answer is a boolean"});
+const {data} = require('../data/flashcardData.json');
+const {cards} = data;
+router.get('/:id', (req, res) => {
+  const {side} = req.query;
+  const {id} = req.params;
+  const text = cards[id][side];
+  const {hint} = cards[id];
+  const templateData = {text,hint}
+  res.render('card',templateData);
 
 })
 module.exports = router;
